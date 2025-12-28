d600cc23258569254b36a4b7ad72a2b80bcce1e6
Alexy Bott <alexisrybin@gmail.com>
2025-12-28 13:46:39 +0300

    feat: add current date display in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
+import {onMounted, ref} from 'vue'
+
+const currentDate = ref('')
+
+onMounted(() => {
+  currentDate.value = new Date().toLocaleDateString('ru-RU', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+})
 </script>
 
 <template>
+  <div>
+    <p> Сегодня: {{ currentDate }}</p>
+  </div>
 </template>
 
 <style scoped>
